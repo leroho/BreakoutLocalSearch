2780304b3baaa5e8f146076a7478ae7a6c9fb116
Adrien Le Roho <adrien.leroho@gmail.com>
2018-12-10 10:51:03 +0100

    Update move.ml

--- src/move.ml
+++ src/move.ml
@@ -1,3 +1,6 @@
+type t = M1 | M2
+(* | M3 | M4 *)
+
 let authorized = fun graph u l->
   List.filter (fun a-> Graph.is_voisin graph a u) l
 
@@ -35,6 +38,53 @@ let create_om = fun graph c ->
   let insert_possible_swap = fun u pqueue->
     List.fold_left (fun a b -> Pqueue.insert (b.Graph.weight -. u.Graph.weight) (u, b) a) pqueue (possible_swap graph u c)in
   List.fold_left (fun a b -> insert_possible_swap b a) om (Graph.SS.elements c)
+
+let eval_move = fun m pa om obj->
+  match m with
+  M1 ->
+  begin
+    let (prio,_,_) = Pqueue.extract pa in
+    obj +. prio
+  end
+  | M2 ->
+  begin
+    let (prio,_,_) = Pqueue.extract om in
+    obj +. prio
+  end
+  (* | M3 ->
+  begin
+    (* let (prio,_,_) = Set.remove x c in
+    obj +. prio *)
+    0.
+  end
+  | M4 ->
+  begin
+    0.
+  end *)
+  
+let best_move = fun pa om obj ->
+    let obj_m1 = eval_move M1 pa om obj in
+    let obj_m2 = eval_move M2 pa om obj in
+    if (obj_m1 < obj_m2) then M2 else M1
+
+let apply_best_move = fun c pa om obj ->
+    let best_m = best_move pa om obj in
+    match best_m with
+    M1 -> 
+    begin
+      let (prio,v,new_pa) = Pqueue.extract pa in
+      c := Graph.SS.add v !c;
+      pa := new_pa;
+      obj := obj +. prio
+    end
+    | M2 ->
+    begin
+      let (prio,v,u,new_om) = Pqueue.extract om in
+      c := Graph.SS.add v !c;
+      c := Set.removeGraph.SS.remove u !c;
+      om := new_om;
+      obj := obj +. prio
+    end
 
     
 (*let ()=
