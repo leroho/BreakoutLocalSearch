4dda5e5201bb299bf51188631f644b4472fc9356
AbdoulayeDiop <33807867+AbdoulayeDiop@users.noreply.github.com>
2019-01-08 10:56:52 +0100

    Add files via upload

--- src/test.ml
+++ src/test.ml
@@ -1,5 +1,6 @@
 let () =
-  for j = 0 to 5 do
+	let n = 100 in
+  for j = 0 to 10 do
     let filename = String.concat "" ["stats";(string_of_int j);".txt"] in
     let dimacs_file = String.concat "" ["data/dimacs_test";(string_of_int j);".txt"] in
     let fic = open_out filename in
@@ -8,21 +9,25 @@ let () =
     let max_val = ref 0 in
     let min_val = ref max_int in
     let sigma = ref 0. in
-    let fbest_array = Array.make 100 0 in
+    let fbest_list = ref [] in
     
-    for i = 0 to 99 do
-      let (fbest, c) = Main.bls demo 1000. in
-      avg := !avg +. (float_of_int fbest);
-      Array.set fbest_array i fbest;
-      min_val := min fbest !min_val;
-      max_val := max fbest !max_val;
-      Printf.fprintf fic "%d %d\n" i fbest;
-      if Main.isClique demo c then Printf.fprintf fic "OK\n" else Printf.fprintf fic "NOT OK\n"
+    for i = 0 to (n-1) do
+		try
+		  (let (fbest, c) = Main.bls demo 1000. in
+		  avg := !avg +. (float_of_int fbest);
+		  fbest_list := fbest::(!fbest_list);
+		  min_val := min fbest !min_val;
+		  max_val := max fbest !max_val;
+		  Printf.fprintf fic "%d %d\n" i fbest;
+		  if Main.isClique demo c then Printf.fprintf fic "OK\n" else Printf.fprintf fic "NOT OK\n")
+		with
+		_ -> ()
     done;
-    avg := !avg /. 100.;
+	let m = List.length !fbest_list in
+    avg := !avg /. (float_of_int m);
     Printf.fprintf fic "\navg = %f" !avg;
-    Array.iter (fun elt -> sigma := !sigma +. ((float_of_int elt) -. !avg)**2.) fbest_array;
-    sigma := sqrt (!sigma /. 100.);
+    List.iter (fun elt -> sigma := !sigma +. ((float_of_int elt) -. !avg)**2.) !fbest_list;
+    sigma := sqrt (!sigma /. (float_of_int m));
     Printf.fprintf fic ",    sigma = %f" !sigma;
     Printf.fprintf fic ",    min = %d" !min_val;
     Printf.fprintf fic ",    max = %d" !max_val;
